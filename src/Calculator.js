95e397d1891a74939ba0ac72fa728a73be62b5d8
Lisa1400 <lisamabe631@gmail.com>
2019-05-31 16:05:49 +0200

    classes, set&get, memory slots

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,18 +1,44 @@
-function add() {
-  let sum = 0;
+class Calculator{
+    constructor () {
+      this.arr = [];
+   } 
   
-  for(let i = 0; i < arguments.length; i++){
-    sum += parseFloat(arguments[i]);
-  }
-  return sum;    
+   
+  set_slot(x){
+    this.arr.push(this.last);
+    return this.arr[x - 1];
   }
 
-  function multiply() {
-    let product = 1;
+  get_slot(){
+    return this.arr;
+  }
+ 
+  add(sum){
+   sum = 0;
     
     for(let i = 0; i < arguments.length; i++){
-      product *= parseFloat(arguments[i]);
+      sum += parseFloat(arguments[i]);
+      this.Last = sum;
     }
-    return product;    
-    }
+    return sum;    
+}
+
+multiply(product) {
+   product = 1;
   
+  for(let i = 0; i < arguments.length; i++){
+    product *= parseFloat(arguments[i]);
+    this.Last = product;
+  }
+  return product;    
+  } 
+
+last(){
+  return this.Last;
+  
+}
+
+}
+let calculator = new Calculator()
+calculator.add(1, 2)
+calculator.multiply(2, 5)
