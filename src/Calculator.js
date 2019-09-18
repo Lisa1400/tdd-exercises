0a5197bd2a625e18ae7c10a41921b7e222f98994
Lisa1400 <lisamabe631@gmail.com>
2019-09-18 11:09:38 +0200

    clean code

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,44 +1,41 @@
-class Calculator{
-    constructor () {
-      this.arr = [];
-   } 
-  
-   
-  set_slot(x){
+class Calculator {
+  constructor() {
+    this.arr = [];
+  }
+
+
+  set_slot(x) {
     this.arr.push(this.last);
     return this.arr[x - 1];
   }
 
-  get_slot(){
+  get_slot() {
     return this.arr;
   }
- 
-  add(sum){
-   sum = 0;
-    
-    for(let i = 0; i < arguments.length; i++){
+
+  add(sum) {
+    sum = 0;
+
+    for (let i = 0; i < arguments.length; i++) {
       sum += parseFloat(arguments[i]);
       this.Last = sum;
     }
-    return sum;    
-}
-
-multiply(product) {
-   product = 1;
-  
-  for(let i = 0; i < arguments.length; i++){
-    product *= parseFloat(arguments[i]);
-    this.Last = product;
+    return sum;
   }
-  return product;    
-  } 
 
-last(){
-  return this.Last;
-  
-}
+  multiply(product) {
+    product = 1;
+
+    for (let i = 0; i < arguments.length; i++) {
+      product *= parseFloat(arguments[i]);
+      this.Last = product;
+    }
+    return product;
+  }
+
+  last() {
+    return this.Last;
+
+  }
 
 }
-let calculator = new Calculator()
-calculator.add(1, 2)
-calculator.multiply(2, 5)
